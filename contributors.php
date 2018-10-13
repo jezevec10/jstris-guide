ba06adedd94b9c4d3a0dda0c95d414d413e50fe3
jezevec10 <jezevec10@gmail.com>
2018-10-13 22:51:39 +0200

    Added contributors metadata file

---
+++ contributors.php
@@ -0,0 +1,19 @@
+<?php
+
+// Contributors & version information
+// If you are creating a translation, you can ignore this file
+
+return [
+    "en" => [
+        "version" => '1.0.0',
+        "translators" => null
+    ],
+    "ko" => [
+        "version" => '1.0.0',
+        "translators" => 'Dinir'
+    ],
+    "fr" => [
+        "version" => '1.0.0',
+        "translators" => 'noelnadal'
+    ],
+];
